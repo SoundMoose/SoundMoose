607b1efff7eee111e6971c1aab1ca9b77e60cf6d
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-04 02:26:14 -0330

    comment for future work

--- src/app/actions/player.ts
+++ src/app/actions/player.ts
@@ -3,6 +3,7 @@ import { Action } from '@ngrx/store';
 
 export class PlayerActions {
 
+  // implement further actions here
 
   static VOLUME_CHANGE = '[Player] Volume Change';
   volumeChange(volume: number): Action {
