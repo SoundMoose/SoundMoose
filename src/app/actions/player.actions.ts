bcacba1ef2d6f9b5e2a063cfd62261ecf2516629
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-04 02:29:37 -0330

    deleted duplicate file

--- src/app/actions/player.actions.ts
+++ src/app/actions/player.actions.ts
@@ -3,20 +3,6 @@ import { Action } from '@ngrx/store';
 
 export class PlayerActions {
 
-  static PLAYING_SONG = '[Player] Playing Song';
-  playingSong(): Action {
-    return {
-      type: PlayerActions.PLAYING_SONG,
-    };
-  }
-
-  static PAUSING_SONG = '[Player] Pausing Song';
-  pausingSong(): Action {
-    return {
-      type: PlayerActions.PAUSING_SONG,
-    };
-  }
-
   static VOLUME_CHANGE = '[Player] Volume Up';
   volumeChange(volume: number): Action {
     return {
