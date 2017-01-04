17668b6468a762d1ce636c6db72a9ca19ef4846f
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2017-01-03 17:40:59 -0800

    Add track listing

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+import { TrackActions } from './track';
+
+export {
+  TrackActions
+};
+
+export default [
+  TrackActions
+];
+=======
 import { SongActions } from './song';
 
 export {
@@ -7,3 +18,4 @@ export {
 export default [
   SongActions
 ];
+>>>>>>> 75dd465d6e2b2155c726dbe3b46b43b07b682109
--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
+export * from './track.model';
+export * from './appstore.model';
+
+=======
 export * from './song';
+>>>>>>> 75dd465d6e2b2155c726dbe3b46b43b07b682109
