d0ed0f8c00c153183d626c48d33639ade7c88b9c
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-06 02:01:32 -0330

    frequency and waveform analysis working for dummyfile MP3 on pageload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app',
@@ -7,4 +7,12 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
+
+
+  // for visualization:
+  // private audioContext: AudioContext;
+  // ngOnInit() {
+  //     this.audioContext = new AudioContext();
+  // }
+
 }
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -22,72 +22,90 @@ import { Player } from './../models/player.model';
 @Injectable()
 export class PlayerService {
 
-   constructor(protected audio: AudioStream, private store$: Store<AppStore>, private playerActions: PlayerActions) {
-     this.store$.select('player')
-       .map((player: Player) => player.currentTrack)
-       .distinctUntilChanged()
-       .subscribe(item => this.play(item.streamUrl));
+  constructor(protected audio: AudioStream, private store$: Store<AppStore>, private playerActions: PlayerActions) {
+    this.store$.select('player')
+     .map((player: Player) => player.currentTrack)
+     .distinctUntilChanged()
+     .subscribe(item => this.play(item.streamUrl));
 
-     this.store$.select('player')
-       .map((player: Player) => player.isPlaying)
-       .distinctUntilChanged()
-       .subscribe(item => !item ? this.pause() : this.play());
+    this.store$.select('player')
+     .map((player: Player) => player.isPlaying)
+     .distinctUntilChanged()
+     .subscribe(item => !item ? this.pause() : this.play());
 
-     this.store$.select('player')
-       .map((player: Player) => player.volume)
-       .distinctUntilChanged()
-       .subscribe(item => this.volume(item));
+    this.store$.select('player')
+     .map((player: Player) => player.volume)
+     .distinctUntilChanged()
+     .subscribe(item => this.volume(item));
 
-     Observable.fromEvent(this.audio, 'timeupdate')
-       .map((item: any) => Math.floor(item.path[0].currentTime))
-       .distinctUntilChanged()
-       .subscribe((item) => this.updateCurrentTime(item));
-   }
+    Observable.fromEvent(this.audio, 'timeupdate')
+     .map((item: any) => Math.floor(item.path[0].currentTime))
+     .distinctUntilChanged()
+     .subscribe((item) => this.updateCurrentTime(item));
+  }
 
-   play(url: string = null): void {
-     if (url) {
-       this.audio.src = url;
-     }
-     this.audio.play();
+  play(url: string = null): void {
+    if (url) {
+      this.audio.src = url;
+    }
+    //  this.audio.play();
 
-    //  //// IN DEVELOPMENT //////////////////////////////////////
-    //  var audioCtx = new AudioContext(),
-    //   audio = new Audio(),
-    //   source,
-    //   // `stream_url` you'd get from
-    //   // requesting http://api.soundcloud.com/tracks/6981096.json
-    //   url = './../assets/sounds/Broke_For_Free_-_01_-_Night_Owl.mp3';
-     //
-    //   audio.src = url;
-    //   source = audioCtx.createMediaElementSource(audio);
-    //   source.connect(audioCtx.destination);
-    //   source.mediaElement.play();
-     //
-    //   var analyser = audioCtx.createAnalyser();
-    //   source.connect(analyser);
-    //   // analyser.connect(distortion);
-     //
-    //   analyser.fftSize = 4096;  // Fast Fourier Transform (fft) in a certain frequency domain,
-    //   var bufferLength = analyser.frequencyBinCount;
-    //   // var dataArray = new Uint8Array(bufferLength);
-    //   var dataArray = new Float32Array(bufferLength); // for Float analyser methods
-     //
-    //   // To capture frequency data, AnalyserNode.getFloatFrequencyData() and AnalyserNode.getByteFrequencyData()
-    //   // To capture Waveform data, and AnalyserNode.getByteTimeDomainData() and AnalyserNode.getFloatTimeDomainData() to capture waveform data.
-    //   analyser.getFloatFrequencyData(dataArray);
-    //   // analyser.getByteFrequencyData(dataArray);
-    //   // analyser.getByteTimeDomainData(dataArray);
-    //   // analyser.getFloatTimeDomainData(dataArray);
-     //
-    //   // console.log('dataArray:', dataArray);
-     //
-    //   setInterval(function() {
-    //     // console.log('Audio audioCtx', audioCtx);
-    //     // console.log('Audio analyser', analyser);
-    //     console.log('dataArray:', dataArray);
-    //   }, 1000);
-    //   //////////////////////////////////////////////////
-   }
+    console.log(this.audio);
+
+///////////////////////////// IN-DEVELOPMENT /////////////////////////////////////
+    let audioCtx = new AudioContext();
+    let newAudio = new Audio();
+    let dummyUrl = './../assets/sounds/Broke_For_Free_-_01_-_Night_Owl.mp3';
+
+    // console.log('newAudio: ', newAudio);
+    // console.log('AudioStream audio: ', this.audio);
+
+    newAudio.src = dummyUrl;
+    // newAudio.src = url;
+    newAudio.crossOrigin = "anonymous"; // CORS :)
+    this.audio.crossOrigin = "anonymous"; // CORS :)
+    // console.log('newArudio.src = ' + newAudio.src);
+
+    let source = audioCtx.createMediaElementSource(newAudio);
+    // let source = audioCtx.createMediaElementSource(this.audio);
+    // source.connect(audioCtx.destination);
+    source.connect(audioCtx.destination);
+    source.mediaElement.play();
+
+    let analyser = audioCtx.createAnalyser();
+    source.connect(analyser);
+    // analyser.connect(distortion);
+
+    analyser.fftSize = 2048;  // Fast Fourier Transform (fft) in a certain frequency domain,
+    let bufferLength = analyser.frequencyBinCount;
+
+    // Byte analyser methods
+    let frequencyDataArray = new Uint8Array(bufferLength);
+    let waveformDataArray = new Uint8Array(bufferLength);
+
+    // Float analyser methods
+    // let frequencyDataArray = new Float32Array(bufferLength);
+    // let waveformDataArray = new Float32Array(bufferLength);
+
+    /// to fix -infinity dataArray bug in frequency, resource: http://stackoverflow.com/questions/14169317/interpreting-web-audio-api-fft-results
+
+    // dataArray = dataArray.map(item=> (item - analyser.minDecibels)*(analyser.maxDecibels - analyser.minDecibels));
+
+    setInterval(function() {
+
+    // Frequency Data:
+    analyser.getByteFrequencyData(frequencyDataArray); // only use this line or the next
+    // analyser.getFloatFrequencyData(frequencyDataArray); // (choose Float or Byte)
+    console.log('frequencyDataArray:', frequencyDataArray);
+
+    // waveform Data:
+    // analyser.getByteTimeDomainData(waveformDataArray);
+    // analyser.getFloatTimeDomainData(waveformDataArray);  // remember to change wafeformDataArray to float!
+    // console.log('waveformDataArray:', waveformDataArray);
+
+  }, 1000);
+///////////////////////// END OF IN-DEVELOPMENT ////////////////////////////////
+  }
 
    pause(): void {
      this.audio.pause();
