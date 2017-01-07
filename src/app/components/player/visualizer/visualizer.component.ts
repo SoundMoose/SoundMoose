f33402d91a1b4a518e56363a960a39b8e018d936
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-07 13:53:50 -0330

    fixed console in browser errors, solution could be more elegant

--- src/app/components/player/visualizer/visualizer.component.ts
+++ src/app/components/player/visualizer/visualizer.component.ts
@@ -25,47 +25,48 @@ export class VisualizerComponent {
   barHeight: number;
   bufferLength: number;
   x: number;
+  hasCanvas: boolean;
 
   constructor (private audioSrc: AudioStream, private store$: Store<AppStore>) {
 
     this.bufferLength = this.audioSrc.bufferLength;
 
-    setInterval(function() {
-      that.frequencyDataArray = that.audioSrc.frequencyDataArray;
-      that.draw();
-    }, 200);
+    // var that = this;
+    this.hasCanvas = false;
 
     var that = this;
+    setInterval(function() {
+      that.frequencyDataArray = that.audioSrc.frequencyDataArray;
+      that.draw(that);
+    }, 200);
+
     window.onload = function() {
-      that.canvas = document.getElementById('visualizerCanvas');
-      // that.canvas = document.querySelector('.visualizer');
-      that.canvasCtx = that.canvas.getContext("2d");
-      that.WIDTH = that.canvas.width;
-      that.HEIGHT = that.canvas.height;
-      that.canvasCtx.clearRect(0, 0, that.WIDTH, that.HEIGHT);
-
-    };
-
+      this.canvas = document.getElementById('visualizerCanvas');
+      // this.canvas = document.querySelector('.visualizer');
+      this.canvasCtx = this.canvas.getContext("2d");
+      this.WIDTH = this.canvas.width;
+      this.HEIGHT = this.canvas.height;
+      this.canvasCtx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
+      this.hasCanvas = true;
+    }.bind(this);
   }
 
-  draw() {
+  draw(context) {
+    if (context.hasCanvas) {
 
-    this.drawVisual = requestAnimationFrame(this.draw);
+      context.drawVisual = requestAnimationFrame(context.draw);
+      context.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      context.canvasCtx.fillRect(0, 0, context.WIDTH, context.HEIGHT);
+      context.barWidth = (context.WIDTH / context.bufferLength) * 2.5;
+      context.barHeight;
+      context.x = 0;
 
-    this.canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-    this.canvasCtx.fillRect(0, 0, this.WIDTH, this.HEIGHT);
-
-    this.barWidth = (this.WIDTH / this.bufferLength) * 2.5;
-    this.barHeight;
-    this.x = 0;
-
-    for(var i = 0; i < this.bufferLength; i++) {
-      this.barHeight = this.frequencyDataArray[i];
-
-      this.canvasCtx.fillStyle = 'rgb(' + (this.barHeight+100) + ',50,50)';
-      this.canvasCtx.fillRect(this.x, this.HEIGHT-this.barHeight/2, this.barWidth, this.barHeight/2);
-
-      this.x += this.barWidth + 1;
+      for(var i = 0; i < context.bufferLength; i++) {
+        context.barHeight = context.frequencyDataArray[i];
+        context.canvasCtx.fillStyle = 'rgb(' + (context.barHeight+100) + ',50,50)';
+        context.canvasCtx.fillRect(context.x, context.HEIGHT-context.barHeight/2, context.barWidth, context.barHeight/2);
+        context.x += context.barWidth + 1;
+      }
     }
   };
 
