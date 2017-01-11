b1e9ca7daddf188a03ec1a91ae25fa33204dc539
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2017-01-10 23:13:37 -0800

    Add comments

--- src/app/reducers/comments.reducer.ts
+++ src/app/reducers/comments.reducer.ts
@@ -1,6 +1,6 @@
 import { Action } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-
+import { TrackDetailsActions } from '../actions/track-details.actions';
 import { Comment } from '../models/coment.model';
 
 export type CommentsState = Comment[];
@@ -9,6 +9,9 @@ const initialState: CommentsState = [];
 
 export default function (state = initialState, action: Action): CommentsState {
   switch (action.type) {
+    case TrackDetailsActions.LOAD_COMMENTS_SUCCESS:
+      return action.payload;
+
     default:
       return state;
   }
