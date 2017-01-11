6d6b1766a2f772265780d0ffa84bf3cea2784526
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-11 03:46:09 -0330

    added github icon and link

--- src/app/audio-element.ts
+++ src/app/audio-element.ts
@@ -79,39 +79,38 @@ export const AUDIO_STREAM_PROVIDER = {
       // filters with type 5 (peaking), which lets all frequencies through and only amplifies/reduce at the frequency at which you've set the respective filter.frequency.value.
 
 
-      masterGain = audioCtx.createGain();
-      masterGain.gain.value = 0;
-
-      // gainDb = 40.0;
-      // gainDb = 0.0;
-      bandSplit = [ 360, 1000, 3600 ];
-
-      lowBand = audioCtx.createBiquadFilter();
-      midBand = audioCtx.createBiquadFilter();
-      highBand = audioCtx.createBiquadFilter();
-
-      lowBand.type = "lowshelf";
-      midBand.type = "peaking";
-      highBand.type = "highshelf";
-
-      lowBand.gain.value = gainDb;
-      midBand.Q.value = 0.5;
-      midBand.gain.value = gainDb;
-      highBand.gain.value = gainDb;
-
-
-      lowBand.frequency.value = bandSplit[0];
-    	midBand.frequency.value = bandSplit[1];
-      highBand.frequency.value = bandSplit[2];
-
-      lowBand.gain.value = 0.6;
-    	midBand.gain.value = 1;
-      highBand.gain.value = 1;
-
-      masterGain.connect(lowBand);
-      lowBand.connect(midBand);
-      midBand.connect(highBand);
-      highBand.connect(audioCtx.destination);
+      // masterGain = audioCtx.createGain();
+      // masterGain.gain.value = 0;
+      //
+      // // gainDb = 40.0;
+      // // gainDb = 0.0;
+      // bandSplit = [ 360, 1000, 3600 ];
+      //
+      // lowBand = audioCtx.createBiquadFilter();
+      // midBand = audioCtx.createBiquadFilter();
+      // highBand = audioCtx.createBiquadFilter();
+      //
+      // lowBand.type = "lowshelf";
+      // midBand.type = "peaking";
+      // highBand.type = "highshelf";
+      //
+      // lowBand.gain.value = gainDb;
+      // // midBand.Q.value = 0.5;
+      // midBand.gain.value = gainDb;
+      // highBand.gain.value = gainDb;
+      //
+      // lowBand.frequency.value = bandSplit[0];
+    	// midBand.frequency.value = bandSplit[1];
+      // highBand.frequency.value = bandSplit[2];
+      //
+      // lowBand.gain.value = 1;
+    	// midBand.gain.value = 1;
+      // highBand.gain.value = 1;
+      //
+      // masterGain.connect(lowBand);
+      // lowBand.connect(midBand);
+      // midBand.connect(highBand);
+      // highBand.connect(audioCtx.destination);
 
       // lowBand.connect( masterGain );
       // midBand.connect( lowBand );
@@ -119,8 +118,8 @@ export const AUDIO_STREAM_PROVIDER = {
       // audioSrcNode.connect(highBand);
       // audioSrcNode.connect(audioCtx.destination);
 
-      audioSrcNode.mediaElement.src = './assets/sounds/Broke_For_Free_-_01_-_Night_Owl.mp3';
-      audioSrcNode.mediaElement.play();
+      // audioSrcNode.mediaElement.src = './assets/sounds/Broke_For_Free_-_01_-_Night_Owl.mp3';
+      // audioSrcNode.mediaElement.play();
 
 ////////// End of In-Development Equalizer Component ////////////////
 
