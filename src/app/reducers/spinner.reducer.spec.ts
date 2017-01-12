05867020aa1c1c7eee18823296bd6f768d1ee614
Doug Vaughn <douglas.s.vaughn@gmail.com>
2017-01-12 12:38:00 -0800

    Fixed unit test.

--- src/app/reducers/spinner.reducer.spec.ts
+++ src/app/reducers/spinner.reducer.spec.ts
@@ -70,7 +70,7 @@ describe('Spinner Reducer', () => {
     let actual = spinnerReducer(state, playerActions.startAudioLoading());
 
     // isSpinning should be true.
-    expect(actual.isSpinning).toBe(true);
+    expect(actual.isLoadSpinning).toBe(true);
   });
 
   it('should set the spinner to false when a track starts to play', () => {
