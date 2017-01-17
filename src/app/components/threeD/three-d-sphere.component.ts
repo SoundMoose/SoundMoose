cf530260735697071b4151eb3c755a859f3b514f
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-17 00:02:25 -0330

    created sphere animation

--- src/app/components/threeD/three-d-sphere.component.ts
+++ src/app/components/threeD/three-d-sphere.component.ts
@@ -86,7 +86,7 @@ export class ThreeDSphereComponent {
     camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
     // camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 0.1, 3000);
 
-    camera.position.set( 0, 100, 500 );
+    camera.position.set( 0, 100, 1000 );
     // scene.add(camera);
 
     // update on resize: renderer size, aspect ratio and projection matrix
@@ -126,11 +126,11 @@ export class ThreeDSphereComponent {
 
     var i, line, vertex1, vertex2, material, p,
     parameters = [
-      [ 0.25, 0xffffff, 1, 2 ],
-      [ 0.5, 0xADBBC2, 1, 1 ],
-      [ 0.75, 0x767676, 0.75, 1 ],
-      [ 1, 0x2E2E2E, 0.5, 1 ],
-      [ 1.25, 0x000833, 0.8, 1 ],
+      [ 0.25, 0xb70000, 1, 2 ], // inner most sphere
+      [ 0.5, 0xffffff, 1, 1 ],
+      [ 0.75, 0xADBBC2, 0.75, 1 ],
+      [ 1, 0x767676, 0.5, 1 ],
+      [ 1.25, 0x2E2E2E, 0.8, 1 ], // outer most sphere
       [ 3.0, 0xaaaaaa, 0.75, 2 ],
       [ 3.5, 0xffffff, 0.5, 1 ],
       [ 4.5, 0xffffff, 0.25, 1 ],
@@ -168,58 +168,63 @@ export class ThreeDSphereComponent {
         return result;
       }
 
+      var getDatBass = getDat(frequencyData, 0, 200) / 50000;
+      var getDatMid1 = getDat(frequencyData, 200, 400) / 40000;
+      var getDatMid2 = getDat(frequencyData, 400, 600) / 30000;
+      var getDatMid3 = getDat(frequencyData, 600, 800) / 20000;
+      var getDatTreble = getDat(frequencyData, 800, 1000) / 10000;
+
       var totalSum = getDat(frequencyData, 0, 1000) / 100000;
-      if (totalSum > 1) {
-        totalSum = 1;
-      }
-      if (totalSum < 0.1) {
-        totalSum = 0.1;
-      }
+      // if (totalSum > 1) { totalSum = 1; }
+      if (totalSum < 0.3) { totalSum = 0.3; }
+
+      // if (getDatBass > 1) { getDatBass = 1; }
+      if (getDatBass < 0.3) { getDatBass = 0.3; }
+
+      // if (getDatMid1 > 1) { getDatMid1 = 1; }
+      if (getDatMid1 < 0.3) { getDatMid1 = 0.3; }
+
+      // if (getDatMid2 > 1) { getDatMid2 = 1; }
+      if (getDatMid2 < 0.3) { getDatMid2 = 0.3; }
+
+      // if (getDatMid3 > 1) { getDatMid3 = 1; }
+      if (getDatMid3 < 0.3) { getDatMid3 = 0.3; }
+
+      // if (getDatTreble > 1) { getDatTreble = 1; }
+      if (getDatTreble < 0.3) { getDatTreble = 0.3; }
 
       //////////////////////////////// Animate ///////////////////////////////
-      //
-      // var i, line, vertex1, vertex2, material, p,
-      // parameters = [
-      //   [ 0.25, 0xff7700, 1, 2 ],
-      //   [ 0.5, 0xff9900, 1, 1 ],
-      //   [ 0.75, 0xffaa00, 0.75, 1 ],
-      //   [ 1, 0xffaa00, 0.5, 1 ],
-      //   [ 1.25, 0x000833, 0.8, 1 ],
-      //   [ 3.0, 0xaaaaaa, 0.75, 2 ],
-      //   [ 3.5, 0xffffff, 0.5, 1 ],
-      //   [ 4.5, 0xffffff, 0.25, 1 ],
-      //   [ 5.5, 0xffffff, 0.125, 1 ]
-      // ];
-      //
-      // _.forEach(geometry.vertices, function(particle, index){
-      //
-      //   for( i = 0; i < parameters.length; ++ i ) {
-      //     p = parameters[ i ];
-      //     material = new THREE.LineBasicMaterial( { color: p[ 1 ], opacity: p[ 2 ], linewidth: p[ 3 ] } );
-      //     line = new THREE.LineSegments( geometry, material );
-      //     line.scale.x = line.scale.y = line.scale.z = p[ 0 ];
-      //     line.originalScale = p[ 0 ];
-      //     line.rotation.y = Math.random() * Math.PI;
-      //     line.updateMatrix();
-      //     scene.add( line );
-      //   }
-      // });
-      //
-      // geometry.verticesNeedUpdate = true;
-      // geometry.colorsNeedUpdate = true;
 
-
-       ////////////// animate motion
 				var time = Date.now() * 0.0001;
 				for ( var i = 0; i < scene.children.length; i ++ ) {
 					var object = scene.children[ i ];
 					if ( object instanceof THREE.Line ) {
-						object.rotation.y = time * ( i < 4 ? ( i + 1 ) : - ( i + 1 ) );
-						if ( i < 5 ) {
+						object.rotation.y = time * ( i < 4 ? ( i + 1 + totalSum ) : - ( i + 1  + totalSum ) );
+						if ( i === 0 ) {
               object.scale.x =
               object.scale.y =
               object.scale.z =
-              object.originalScale * totalSum * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
+              object.originalScale * getDatBass * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
+            } else if ( i === 1 ) {
+              object.scale.x =
+              object.scale.y =
+              object.scale.z =
+              object.originalScale * getDatMid1 * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
+            } else if ( i === 2 ) {
+              object.scale.x =
+              object.scale.y =
+              object.scale.z =
+              object.originalScale * getDatMid2 * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
+            } else if ( i === 3 ) {
+              object.scale.x =
+              object.scale.y =
+              object.scale.z =
+              object.originalScale * getDatMid3 * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
+            } else if ( i === 4 ) {
+              object.scale.x =
+              object.scale.y =
+              object.scale.z =
+              object.originalScale * getDatTreble * (i/5+1) * (1 + 0.5 * Math.sin( 7*time ));
             }
 					}
 				}
