e86c7892955b98e30ed32dc2d6e4d5a095a4d4f5
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2017-01-16 20:37:35 -0800

    Work around auth0 issues by setting useHash to false

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -134,7 +134,7 @@ const store = compose(...metaReducers)({
     HttpModule,
     MaterialModule.forRoot(),
     FormsModule,
-    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(ROUTES, { useHash: false, preloadingStrategy: PreloadAllModules })
   ],
   providers: [ // expose our Services and Providers into Angular's dependency injection
     ENV_PROVIDERS,
