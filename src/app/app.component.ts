5a3368e125aa346f04c798ddcd9b7f02e06fd73b
Stefan Ruijsenaars <stefanruijsenaars@users.noreply.github.com>
2017-01-18 21:20:59 -0800

    make the footer work again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ import { SoundmooseUser } from './models/soundmoose-user.model';
 import { AppStore } from './models/appstore.model';
 import { AudioControlsActions } from './actions/audio-controls.actions';
 import { AudioControls } from './models/audio-controls.model';
-import { Auth } from './services/auth.service';
 
 @Component({
   selector: 'app',
@@ -22,7 +21,7 @@ export class AppComponent {
   toggleFrequencyOrWaveform$: Observable<boolean>;
   soundmooseUser$: Observable<SoundmooseUser>;
 
-  constructor( private store$: Store<AppStore>, private AudioControlsActions: AudioControlsActions, private auth: Auth) {
+  constructor( private store$: Store<AppStore>, private AudioControlsActions: AudioControlsActions) {
     this.soundmooseUser$ = this.store$.select(s => s.soundmooseUser);
     this.toggleFrequencyOrWaveform$ = this.store$.select('audiocontrols')
       .map((audiocontrols: AudioControls) => audiocontrols.toggleFrequencyOrWaveform)
