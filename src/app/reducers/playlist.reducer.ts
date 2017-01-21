b2e28d4a2edc0dd44d76478019fe125304ee6dc6
Cal Holloway <calvin.c.holloway@gmail.com>
2017-01-20 23:03:15 -0330

    removed lingering console.log for merge

--- src/app/reducers/playlist.reducer.ts
+++ src/app/reducers/playlist.reducer.ts
@@ -50,7 +50,6 @@ export default function (state = initialState, action: Action): PlaylistState {
       let newState = Object.assign({}, state, {
         tracks: newTracks
       });
-      console.log('state has been changed for playlists, new tracks state = ', state.tracks);
       return newState;
     }
 
